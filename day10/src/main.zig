21639e30c2d60d5760ccbddde6056bd887738dda
Sanford Pun <punsanford@gmail.com>
2022-12-10 22:02:52 +0000

    Finish Day 10 P2

--- day10/src/main.zig
+++ day10/src/main.zig
@@ -7,7 +7,7 @@ const fs = std.fs;
 
 const print = std.debug.print;
 
-fn solveP1(x_deltas: ArrayList(i32)) !i32 {
+fn solveP1(x_deltas: ArrayList(i32)) i32 {
     var x_val: i32 = 1;
     var cycle_idx: u16 = 0;
     var acc: i32 = 0;
@@ -27,6 +27,34 @@ fn solveP1(x_deltas: ArrayList(i32)) !i32 {
     return acc;
 }
 
+fn solveP2(x_deltas: ArrayList(i32)) void {
+    const WIDTH = 40;
+    const HEIGHT = 6;
+
+    var sprite_middle_pos: i32 = 1;
+    var row_idx: u16 = 0;
+    while (row_idx < HEIGHT) {
+        var col_idx: u16 = 0;
+        while (col_idx < WIDTH) {
+            // Start cycle
+
+            // During cycle
+            if (sprite_middle_pos - 1 <= col_idx and col_idx <= sprite_middle_pos + 1) {
+                print("#", .{});
+            } else {
+                print(".", .{});
+            }
+
+            // End of cycle
+            sprite_middle_pos += x_deltas.items[1 + row_idx * WIDTH + col_idx];
+            
+            col_idx += 1;
+        }
+        print("\n", .{});
+        row_idx += 1;
+    }
+}
+
 pub fn main() !void {
     // const filename = "dummy_input.txt";
     const filename = "input.txt";
@@ -44,7 +72,6 @@ pub fn main() !void {
     while (try in_stream.readUntilDelimiterOrEof(&buf, '\n')) |line| {
         if (line[0] == 'a') {
             const delta = try fmt.parseInt(i32, line[5..], 10);
-
             try x_deltas.append(0); // wait 1 cycle
             try x_deltas.append(delta);
         } else if (line[0] == 'n') {
@@ -52,5 +79,7 @@ pub fn main() !void {
         }
     }
     
-    print("Part 1: {d}\n", .{try solveP1(x_deltas)});
+    print("Part 1: {d}\n", .{solveP1(x_deltas)});
+    print("Part 2:\n", .{});
+    solveP2(x_deltas);
 }
